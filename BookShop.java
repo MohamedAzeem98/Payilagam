89a67e4aa975d0ce67b7b60e236cd4796b2c509c
MohamedAzeem <azeemerv@gmail.com>
2023-08-25 11:14:30 +0530

    second project

---
+++ BookShop.java
@@ -0,0 +1,20 @@
+public class BookShop {
+	
+	static String name="Mohamed";
+	static int doorNo=40;
+	
+	String bookName="The Jungle Book";
+	
+public static void main(String [] args){
+			
+			System.out.println(BookShop.name);
+		  
+		  	System.out.println(BookShop.doorNo);
+			
+		  BookShop book1 = new BookShop();
+		  
+		//BookShop book1=new BookShop();
+	
+		System.out.println(book1.bookName);
+			}
+}
