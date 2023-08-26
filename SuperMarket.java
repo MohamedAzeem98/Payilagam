608b72ddf80fb5fd54c9c6084cec2c942a1d8b6e
MohamedAzeem <azeemerv@gmail.com>
2023-08-26 08:06:11 +0530

    Second commit

---
+++ SuperMarket.java
@@ -0,0 +1,25 @@
+public class  SuperMarket {
+
+		String ItemName1 = " Sugar";												// Instance variables
+		int price1=55;
+		
+		String ItemName2 = "BoostPowder";								// Instance variables
+		int price2 = 199;
+		
+	  public static void main(String  args [ ]  ){                                //Main_Method
+		
+		SuperMarket object1=new SuperMarket();                       //Object1 is created
+		
+		System.out.println(object1.ItemName1);
+		
+		  System.out.println(object1.price1);
+		
+		SuperMarket object2=new SuperMarket();                      //Object2 is created
+		
+		System.out.println(object2.ItemName2);
+		
+		  System.out.println(object2.price2);
+			
+	  			}
+		
+		}
