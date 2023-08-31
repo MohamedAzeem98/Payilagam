5f34caa726ae307133cecdfb872d036d31fc9b34
MohamedAzeem <azeemerv@gmail.com>
2023-08-31 09:34:58 +0530

    Fourth commit

---
+++ Home1.java
@@ -0,0 +1,24 @@
+public class Home1 {
+
+	static String name;  //null
+	static int doorNo;  //0
+	String name1; //null
+	int age;//null
+	float weight;//0.0
+	boolean student;//false
+	
+public static void main(String args[]){
+
+System.out.println(Home1.name);
+System.out.println(Home1.doorNo);
+
+	Home1 person1=new Home1();
+	
+	System.out.println("person1.name = "+person1.name1);
+	System.out.println("Person1.age ="+person1.age);
+	System.out.println("Person1.weight="+person1.weight);
+	System.out.println("Person1.student="+person1.student);
+	
+     }
+
+}
