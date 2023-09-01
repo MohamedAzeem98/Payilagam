4eefd6cefbb79834a181d9af89dc46e548b6b9c2
MohamedAzeem <azeemerv@gmail.com>
2023-09-01 11:10:35 +0530

    return type

---
+++ SuperMarket3.java
@@ -0,0 +1,19 @@
+public class SuperMarket3{
+public static int main(String args[]){
+	SuperMarket3 sm=new SuperMarket3();
+
+	int re=sm.add();   
+	System.out.println(re);
+	
+     }
+	int add(){
+		
+		int no1=45;
+		int no2=50;
+		int result=no1+no2;
+		System.out.println(result);
+		return result;
+		
+		
+	}
+}
