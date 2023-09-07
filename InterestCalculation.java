8636f8d07586c8ade2737923db2b7c829b3cc280
MohamedAzeem <azeemerv@gmail.com>
2023-09-07 20:39:53 +0530

    Packages concept

---
+++ InterestCalculation.java
@@ -0,0 +1,17 @@
+package com.indianBank.deposit;                                    //package - UseDefined
+
+public class InterestCalculation {
+
+	public static void main(String args[]){
+	
+	InterestCalculation ic = new InterestCalculation();
+		
+		ic.calculate();
+	}
+	
+	public void calculate(){
+		
+		System.out.println("5 % interest");
+	}
+	
+	}
