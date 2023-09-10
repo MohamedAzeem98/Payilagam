cd3986d8175fd74f53a91fe9ddadfaf7b373aa16
MohamedAzeem <azeemerv@gmail.com>
2023-09-10 20:12:12 +0530

    package-2 concept

---
+++ IndianBank.java
@@ -0,0 +1,13 @@
+package com.indianBank;
+import com.indianBank.deposit.InterestCalculation;
+
+public class IndianBank {
+ 		
+	public static void main(String args[]){
+		
+		InterestCalculation ic = new InterestCalculation();
+		
+		ic.calculate(10);
+		
+	     }
+	}					
