1f7dbcc779f015c126140f831cd1ad569a7ed2d3
MohamedAzeem <azeemerv@gmail.com>
2023-09-11 20:41:03 +0530

    inheritance with method overriding

---
+++ Youngster.java
@@ -0,0 +1,19 @@
+package salem;
+public class Youngster extends parent{
+		public static void main(String args[]){
+	
+
+	Youngster yy=new Youngster();
+	
+	yy.gardening();
+	
+	yy.fixMarriage();
+	
+			}
+			public void fixMarriage(){                                                    //Method overriding
+			System.out.println("Marriage -- opinion ");
+			
+			}
+			
+			
+	}
