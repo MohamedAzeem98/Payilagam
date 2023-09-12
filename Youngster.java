51da12309088ec6c9a5251b55f9b924f56e843d9
MohamedAzeem <azeemerv@gmail.com>
2023-09-12 19:40:44 +0530

    inheritance

--- Youngster.java
+++ Youngster.java
@@ -8,11 +8,21 @@ public class Youngster extends parent{
 	yy.gardening();
 	
 	yy.fixMarriage();
+		
+	yy.takeRest();
+			
+		System.out.println(yy.age);
 	
 			}
-			public void fixMarriage(){                                                    //Method overriding
+			public void fixMarriage(){                                                    //Method overriding     
+				super.fixMarriage();        // calling same method from parent class
 			System.out.println("Marriage -- opinion ");
+				Youngster.study();          //calling static method in instance method of current class
 			
+			}
+	
+			public static void study(){
+				System.out.println("studying");
 			}
 			
 			
