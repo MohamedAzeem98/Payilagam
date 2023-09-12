c8c689e8ec760f7864fd0e0863a9c9b2bd53e1aa
MohamedAzeem <azeemerv@gmail.com>
2023-09-12 19:39:17 +0530

    inheritance

--- parent.java
+++ parent.java
@@ -1,15 +1,22 @@
 package salem;
-public class parent {
+public class parent extends GrandParent{
+	
+	int age=45;
+	private int savings=10000;
+	
 	
 	public void gardening(){
-	System.out.println("gardening");
+	
+		System.out.println("gardening");
 	}
+	
 	public void fixMarriage(){
 		System.out.println("fixing - marriage rules");
 	}
 	
+	public void takeRest(){
+		System.out.println("Parent  - takeRest");
+	}
 	
 	
-	
-	
-}
+	}                                                                                                                                             
