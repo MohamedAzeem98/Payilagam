d93b0744b79ea718ad7f150137eb6a4ebaf6f796
MohamedAzeem <azeemerv@gmail.com>
2023-09-21 19:51:03 +0530

    Dynamic Binding

---
+++ Dada.java
@@ -0,0 +1,12 @@
+public class Dada {                    //Dynamic binding -ParentClass
+	
+	final int amount=1654626456;
+	public void profession(){
+		System.out.println("Running Business");
+	}
+	
+	public void bankBalance(){
+		System.out.println(amount);
+	}
+	
+}
