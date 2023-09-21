77c5c6006833c7ccb8ecb25376971168527f0601
MohamedAzeem <azeemerv@gmail.com>
2023-09-21 19:49:03 +0530

    abstract class

---
+++ Car.java
@@ -0,0 +1,12 @@
+public abstract class Car {                                                        //abstract class 
+	
+	public void show(){                                                   //implemented method 
+	System.out.println("The Car colour is blue");
+	}
+	
+	public abstract void mechanism();                   //unimplemented method
+	
+	public abstract void axleDesign();                   //unimplemented method
+	
+	
+}
