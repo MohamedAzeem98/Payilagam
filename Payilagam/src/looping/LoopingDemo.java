fee9c1a382f08db3ff5973bc910ef0b1bf3d6896
MohamedAzeem <azeemerv@gmail.com>
2023-09-28 16:53:30 +0530

    whilelooping

---
+++ Payilagam/src/looping/LoopingDemo.java
@@ -0,0 +1,16 @@
+package looping;
+
+public class LoopingDemo {
+	
+	public static void main(String[] args) {
+		
+		int count=0;
+		
+		while(count<5) {
+			System.out.print(1+" ");
+			count=count+1;
+		}
+		
+	}
+
+}
