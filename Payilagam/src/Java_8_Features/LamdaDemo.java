d08eb47ba2a1068beed8196836455c09414b1afa
MohamedAzeem <azeemerv@gmail.com>
2023-10-04 21:10:55 +0530

    lamda expression for functional interface

---
+++ Payilagam/src/Java_8_Features/LamdaDemo.java
@@ -0,0 +1,50 @@
+package Java_8_Features;
+
+public class LamdaDemo {
+	public static void main(String[] args) {
+		
+		
+		
+ Functional_Interface_Demo fd=            //type 2
+			(int a,int b)->{
+				System.out.println(a+b);
+			};
+	fd.add(5,6);	
+	
+	}
+}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		/*Functional_Interface_Demo fd=           //type 1
+				()->{
+				System.out.println("Hello");	
+				};
+				
+		fd.add();	
+		*/
+	
+
+		
+		
+		
+		
+		
+		
+	/*	Functional_Interface_Demo fd=            //type 3
+		(int a,int b)->{
+		return a+b;	
+		};
+       System.out.println(fd.add(5,6));	
+    */   
+		
+						
+			
+	
