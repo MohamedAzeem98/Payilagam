e6358a475b8864b9187a61f45c683c5f52af3855
MohamedAzeem <azeemerv@gmail.com>
2023-10-05 21:27:12 +0530

    ArrayclassRoompractice

---
+++ Payilagam/src/LearnArray/Array_Demo.java
@@ -0,0 +1,20 @@
+package LearnArray;
+
+public class Array_Demo {
+	public static void main(String[] args) {
+		
+		int marks[]= {78,98,79,80,35};    //task1->total , task2->average , task--->pass/fail
+	
+		for(int i=0;i<=marks.length-1;i+=2) {
+			System.out.println(marks[i]);
+			}
+			
+		}
+	
+		
+		
+		
+		
+	}
+
+
